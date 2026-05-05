94fc8f0269380a8bdf647109789d43cef6789207
Mallesh-1124 <kmallesh.1124@gmail.com>
2026-05-05 13:02:41 +0530

    Add collectstatic to entrypoint for WhiteNoise

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Running migrations..."
 python manage.py migrate --noinput
 
